2694c1fac2e3b30325e0e0cd5e214d5e63285c58
Aiden <aidenb@hotmail.co.uk>
2025-08-03 23:35:41 +0100

    Local work before pull

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+from pokedex import analyse_pokemon  # Your Python logic
+
+app = Flask(__name__)
+
+import os
+port = int(os.environ.get("PORT", 10000))
+
+
+@app.route("/")
+def home():
+    print("Current working directory:", os.getcwd())
+    print("Templates folder contents:", os.listdir("templates"))
+    return render_template("index.html")
+
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    query = request.form["query"]  # Pokémon name or number
+    move_type = request.form.get("move_type", None)  # default to None
+    result = analyse_pokemon(query, move_type)  # Pass both to logic
+    return render_template("index.html", result=result)
+
+@app.route("/defend", methods=["POST"])
+def defend():
+    query = request.form["query"]
+    move_type = request.form["move_type"]
+    result = analyse_pokemon(query, move_type)
+    return render_template("index.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
+
